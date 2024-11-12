314685dfa1d62207f04d03fc58bac96469474390
lewpar <lewis.o.parmenter@gmail.com>
2024-11-13 09:52:45 +1100

    Added buzzer.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import machine
 import time
 import dht
 
+PIN_BUZZER = 32
+
 PIN_LED_GREEN = 33
 PIN_LED_ORANGE = 25
 PIN_LED_RED = 26
@@ -13,9 +15,11 @@ PIN_BUTTON = 14
 use_offboard = True
 double_press = False
 
-pin_green = machine.Pin(PIN_LED_GREEN, machine.Pin.OUT)
-pin_orange = machine.Pin(PIN_LED_ORANGE, machine.Pin.OUT)
-pin_red = machine.Pin(PIN_LED_RED, machine.Pin.OUT)
+buzzer = machine.PWM(machine.Pin(PIN_BUZZER))
+
+led_green = machine.Pin(PIN_LED_GREEN, machine.Pin.OUT)
+led_orange = machine.Pin(PIN_LED_ORANGE, machine.Pin.OUT)
+led_red = machine.Pin(PIN_LED_RED, machine.Pin.OUT)
 
 button = machine.Pin(PIN_BUTTON)
 
@@ -45,6 +49,22 @@ def get_offboard_temp():
     temp_sensor.measure() 
     return temp_sensor.temperature()
 
+def get_temp():
+    global last_temp, last_offboard_temp, use_offboard
+
+    onboard_temp = to_celcius(get_onboard_temp())
+    offboard_temp = get_offboard_temp()
+
+    if not is_close(last_temp, onboard_temp):
+        print(f"Onboard temperature changed from {last_temp} to {onboard_temp}.")
+        last_temp = onboard_temp
+
+    if not is_close(last_offboard_temp, offboard_temp):
+        print(f"Offboard temperature changed from {last_offboard_temp} to {offboard_temp}.")
+        last_offboard_temp = offboard_temp
+
+    return offboard_temp if use_offboard else onboard_temp
+
 def is_close(a, b):
     """
     Used to calculate if floating point numbers are the same using an epsilon.
@@ -66,33 +86,30 @@ def button_pressed(pin):
     use_offboard = not use_offboard
     print(f"Button on {pin} pressed, changing sensor to {"offboard" if use_offboard else "onboard"}")
 
+
+def buzz(frequency: int, duration_ms: int, break_ms: int):
+    buzzer.init(freq=frequency)
+    time.sleep_ms(duration_ms)
+    buzzer.deinit()
+    time.sleep_ms(break_ms)
+
 button.irq(trigger=machine.Pin.IRQ_FALLING, handler=button_pressed)
 
 while True:
-    onboard_temp = to_celcius(get_onboard_temp())
-    offboard_temp = get_offboard_temp()
+    temp = get_temp()
 
-    if not is_close(last_temp, onboard_temp):
-        print(f"Onboard temperature changed from {last_temp} to {onboard_temp}.")
-        last_temp = onboard_temp
-
-    if not is_close(last_offboard_temp, offboard_temp):
-        print(f"Offboard temperature changed from {last_offboard_temp} to {offboard_temp}.")
-        last_offboard_temp = offboard_temp
-
-    temp =  offboard_temp if use_offboard else onboard_temp
-
-    if temp <= 27:
-        pin_green.on()
-        pin_orange.off()
-        pin_red.off()
-    elif temp > 27 and temp <= 28:
-        pin_green.off()
-        pin_orange.on()
-        pin_red.off()
-    elif temp > 28:
-        pin_green.off()
-        pin_orange.off()
-        pin_red.on()
-
-    time.sleep_ms(250)
+    if temp <= 24:
+        led_green.on()
+        led_orange.off()
+        led_red.off()
+        buzz(1000, 100, 500)
+    elif temp > 24 and temp <= 25:
+        led_green.off()
+        led_orange.on()
+        led_red.off()
+        buzz(1000, 100, 250)
+    elif temp > 25:
+        led_green.off()
+        led_orange.off()
+        led_red.on()
+        buzz(1000, 100, 100)
